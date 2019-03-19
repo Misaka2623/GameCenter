f0fd16420e11d370c9a573efe35f00a56505bbae
misaka2623 <415470991@qq.com>
2019-03-19 15:00:22 -0700

    initialize project

---
+++ fifteen_puzzle.js
@@ -0,0 +1,20 @@
+(function () {
+  let SIZE = 4;
+
+  window.addEventListener("load", main);
+
+  function main() {
+    let containerDiv = document.createElement("div");
+    containerDiv.id = "container";
+    document.body.appendChild(containerDiv);
+    generatePuzzle();
+  }
+
+  function generatePuzzle() {
+    for (let col = 0; col < SIZE; col++) {
+      for (let row = 0; row < SIZE; row++) {
+        
+      }
+    }
+  }
+})();
