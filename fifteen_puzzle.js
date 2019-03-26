80048cbd61060ee1cd6e4aad00aa522b2af1e2f9
misaka2623 <415470991@qq.com>
2019-03-26 14:54:53 -0700

    shuffle board

--- fifteen_puzzle.js
+++ fifteen_puzzle.js
@@ -51,8 +51,16 @@
           this.addSquare(x, y);
         }
       }
+
+      this.refreshBoard();
     }
 
+    /**
+     * Adds a square into the board.
+     *
+     * @param x {number} the x coordinate of the square.
+     * @param y {number} the y coordinate of the square.
+     */
     addSquare(x, y) {
       const ordinal = x + y * BOARD_SIZE;
 
@@ -63,6 +71,10 @@
         if (this.isAbleToMove(ordinal)) {
           this.swap(this.indexOf(ordinal),
               this.indexOf(this.orders_.length - 1));
+
+          if (this.isCompleted()) {
+            setTimeout(() => window.alert('win'), 200);
+          }
         }
       });
 
@@ -84,8 +96,6 @@
           square.style.borderColor = 'black';
         });
       }
-
-      document.getElementById('board').appendChild(square);
 
       this.data_.push(square);
     }
@@ -111,15 +121,19 @@
      * @returns {boolean} true if the specified square is able to move.
      */
     isAbleToMove(order) {
-      const index1 = this.indexOf(order);
-      const index2 = this.indexOf(this.orders_.length - 1);
+      if (this.isLegalPosition(order)) {
+        const index1 = this.indexOf(order);
+        const index2 = this.indexOf(this.orders_.length - 1);
 
-      const x1 = index1 % BOARD_SIZE;
-      const y1 = parseInt(index1 / BOARD_SIZE);
-      const x2 = index2 % BOARD_SIZE;
-      const y2 = parseInt(index2 / BOARD_SIZE);
+        const x1 = index1 % BOARD_SIZE;
+        const y1 = parseInt(index1 / BOARD_SIZE);
+        const x2 = index2 % BOARD_SIZE;
+        const y2 = parseInt(index2 / BOARD_SIZE);
 
-      return Math.abs(x1 - x2) + Math.abs(y1 - y2) === 1;
+        return Math.abs(x1 - x2) + Math.abs(y1 - y2) === 1;
+      } else {
+        return false;
+      }
     }
 
     /**
@@ -134,6 +148,13 @@
       this.orders_.splice(index2, 1, item1);
       this.orders_.splice(index1, 1, item2);
 
+      this.refreshBoard();
+    }
+
+    /**
+     * Refreshes the board to match the change.
+     */
+    refreshBoard() {
       const container = document.getElementById('board');
       container.innerHTML = '';
       for (let order of this.orders_) {
@@ -151,6 +172,57 @@
 
       this.data_[this.orders_.length - 1].style.borderColor = colors;
     }
+
+    /**
+     * Shuffles the squares on the board.
+     */
+    shuffle() {
+      for (let i = 0; i < 1000; i++) {
+        const index = this.indexOf(this.orders_.length - 1);
+        const directions = [];
+        if (parseInt(index / BOARD_SIZE) !== 0) {
+          directions.push(index - BOARD_SIZE);
+        }
+        if (parseInt(index / BOARD_SIZE) !== BOARD_SIZE - 1) {
+          directions.push(index + BOARD_SIZE);
+        }
+        if (index % BOARD_SIZE !== 0) {
+          directions.push(index - 1);
+        }
+        if (index % BOARD_SIZE !== BOARD_SIZE - 1) {
+          directions.push(index + 1);
+        }
+
+        const random = parseInt(Math.random() * directions.length);
+        const target = directions[random];
+        this.swap(target, index);
+      }
+    }
+
+    /**
+     * Indicates whether the position is a legal position on the board.
+     *
+     * @param position {number} the position on the board.
+     * @returns {boolean} true if the position is a legal position on the board.
+     */
+    isLegalPosition(position) {
+      return 0 <= position && position < this.orders_.length;
+    }
+
+    /**
+     * Indicates whether all squares on the board are at the correct position.
+     *
+     * @returns {boolean} true if all squares on the board are at the correct
+     * position.
+     */
+    isCompleted() {
+      for (let i = 1; i < this.orders_.length; i++) {
+        if (this.orders_[i - 1] + 1 !== this.orders_[i]) {
+          return false;
+        }
+      }
+      return true;
+    }
   }
 
   window.addEventListener('load', main);
@@ -160,5 +232,6 @@
     container.style.width = `${BOARD_SIZE * SQUARE_SIZE}px`;
     container.style.height = `${BOARD_SIZE * SQUARE_SIZE}px`;
     const board = new Board();
+    board.shuffle();
   }
 })();
