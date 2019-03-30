c18ae6e1eace20f34beed484889a17d3fb422631
misaka2623 <415470991@qq.com>
2019-03-30 16:57:06 -0700

    add reset button and stage select label (no effect)

--- fifteen_puzzle.js
+++ fifteen_puzzle.js
@@ -1,19 +1,17 @@
 (function() {
   'use strict';
 
-  // TODO: reset game
-
-  /**
-   * The beginning time of the game.
-   * @type {number}
-   */
-  let aStartTime;
-
   /**
    * The number for stopping the timer.
    * @type {number}
    */
   let aNumber;
+
+  /**
+   * The beginning time of the game (in ms).
+   * @type {number}
+   */
+  let aStartTime;
 
   /**
    * A storage for storing the scores.
@@ -70,20 +68,20 @@
    */
   class Board {
     /**
-     * The number of squares at each row and column on the game board.
-     * @const
-     * @type {number}
-     * @private
-     */
-    static _size = 2;
-
-    /**
      * The width and height of the game board (in px).
      * @const
      * @type {number}
      * @private
      */
     static _board_size = 400;
+
+    /**
+     * The number of squares at each row and column on the game board.
+     * @const
+     * @type {number}
+     * @private
+     */
+    static _size = 2;
 
     /**
      * The total number of squares on the puzzle board.
@@ -118,35 +116,6 @@
     static _square_font_size = Board._square_size * 0.4;
 
     /**
-     * Constructs a Board.
-     */
-    constructor() {
-      /**
-       * An array contains the order of the ordinal of squares on the game
-       * board.
-       * @const
-       * @type {number[]}
-       * @private
-       */
-      this._ordinates = [];
-
-      /**
-       * Stores all html elements of squares on the game board.
-       * @const
-       * @type {Map<number, HTMLDivElement>}
-       * @private
-       */
-      this._data = new Map();
-
-      const container = document.getElementById('board');
-      container.style.height = `${Board._board_size}px`;
-      container.style.width = `${Board._board_size}px`;
-
-      this._initializeSquares();
-      this._show();
-    }
-
-    /**
      * Translates the specified ordinate to coordinate.
      * @param {number} ordinate the ordinate which should be translated.
      * @returns {number[]} an array that contains two numbers. The first number
@@ -158,39 +127,45 @@
     }
 
     /**
-     * Indicates whether the puzzle is solved.
-     * @returns {boolean} true if the puzzle is solved.
+     * Stores all html elements of squares on the game board.
+     * @const
+     * @type {Map<number, HTMLDivElement>}
+     * @private
      */
-    isSolved() {
-      return this._ordinates.filter(
-          (value, index) => value !== index + 1).length === 0;
-    }
+    _data;
 
     /**
-     * Starts the game.
+     * An array contains the order of the ordinal of squares on the game
+     * board.
+     * @const
+     * @type {number[]}
+     * @private
      */
-    startGame() {
-      aStartTime = Date.now();
-      while (this.isSolved()) {
-        this._shuffle();
-      }
-      aNumber = setInterval(countTime, 1000);
-      document.getElementById('start-game').disabled = true;
-    }
+    _ordinates;
 
     /**
-     * Ends the game.
+     * Constructs a Board.
      */
-    endGame() {
-      clearInterval(aNumber);
-      const time = Date.now() - aStartTime;
-      ScorePad.addScores(time);
-      setTimeout(() => window.alert(
-          `complete! used ${getTimeString(time)}`), 200);
-      document.getElementById('timer').value = '0:00:00';
-      document.getElementById('start-game').disabled = false;
+    constructor() {
+      this._data = new Map();
+      this._ordinates = [];
+
+      const container = document.getElementById('board');
+      container.style.height = `${Board._board_size}px`;
+      container.style.width = `${Board._board_size}px`;
+
       this._initializeSquares();
       this._show();
+    }
+
+    /**
+     * Gets the ordinate of the square at specified position.
+     * @param {number} index position that the square at.
+     * @returns {number} the ordinate of the square.
+     * @private
+     */
+    _index2ordinate(index) {
+      return this._ordinates[index];
     }
 
     /**
@@ -272,6 +247,32 @@
     }
 
     /**
+     * Indicates whether the specified square is available to move.
+     * @param {number} ordinate the ordinate of the square.
+     * @returns {boolean} true if the square is available to move.
+     * @private
+     */
+    _isAvailableToMove(ordinate) {
+      const index = this._ordinate2index(ordinate);
+      const empty = this._ordinate2index(Board._length);
+      return index >= 0 && index + Board._size === empty
+          || index % Board._size !== 0 && index - 1 === empty
+          || index < Board._length && index - Board._size === empty
+          || index % Board._size !== Board._size - 1 && index + 1 === empty;
+    }
+
+    /**
+     * Translates the specified ordinate to the index that the square at.
+     *
+     * @param {number} ordinate the ordinate of the square.
+     * @returns {number} the index that the square at.
+     * @private
+     */
+    _ordinate2index(ordinate) {
+      return this._ordinates.indexOf(ordinate);
+    }
+
+    /**
      * Shows the game board depending on current set of squares.
      * @private
      */
@@ -291,55 +292,6 @@
       colors += index % Board._size === 0 ? 'white' : 'black';
 
       this._data.get(Board._length).style.borderColor = colors;
-    }
-
-    /**
-     * Swaps the two squares at specified position.
-     * @param {number} index1 the index of one square.
-     * @param {number} index2 the index of another square.
-     * @private
-     */
-    _swap(index1, index2) {
-      const item1 = this._index2ordinate(index1);
-      const item2 = this._index2ordinate(index2);
-      this._ordinates.splice(index2, 1, item1);
-      this._ordinates.splice(index1, 1, item2);
-    }
-
-    /**
-     * Translates the specified ordinate to the index that the square at.
-     *
-     * @param {number} ordinate the ordinate of the square.
-     * @returns {number} the index that the square at.
-     * @private
-     */
-    _ordinate2index(ordinate) {
-      return this._ordinates.indexOf(ordinate);
-    }
-
-    /**
-     * Gets the ordinate of the square at specified position.
-     * @param {number} index position that the square at.
-     * @returns {number} the ordinate of the square.
-     * @private
-     */
-    _index2ordinate(index) {
-      return this._ordinates[index];
-    }
-
-    /**
-     * Indicates whether the specified square is available to move.
-     * @param {number} ordinate the ordinate of the square.
-     * @returns {boolean} true if the square is available to move.
-     * @private
-     */
-    _isAvailableToMove(ordinate) {
-      const index = this._ordinate2index(ordinate);
-      const empty = this._ordinate2index(Board._length);
-      return index >= 0 && index + Board._size === empty
-          || index % Board._size !== 0 && index - 1 === empty
-          || index < Board._length && index - Board._size === empty
-          || index % Board._size !== Board._size - 1 && index + 1 === empty;
     }
 
     /**
@@ -369,14 +321,56 @@
       }
       this._show();
     }
-  }
 
-  window.addEventListener('load', () => {
-    const board = new Board();
-    const button = document.getElementById('start-game');
-    button.addEventListener('click', () => board.startGame());
-    ScorePad.show();
-  });
+    /**
+     * Swaps the two squares at specified position.
+     * @param {number} index1 the index of one square.
+     * @param {number} index2 the index of another square.
+     * @private
+     */
+    _swap(index1, index2) {
+      const item1 = this._index2ordinate(index1);
+      const item2 = this._index2ordinate(index2);
+      this._ordinates.splice(index2, 1, item1);
+      this._ordinates.splice(index1, 1, item2);
+    }
+
+    /**
+     * Ends the game.
+     */
+    endGame() {
+      clearInterval(aNumber);
+      const time = Date.now() - aStartTime;
+      ScorePad.addScores(time);
+      setTimeout(() => window.alert(
+          `complete! used ${getTimeString(time)}`), 200);
+      document.getElementById('timer').value = '0:00:00';
+      document.getElementById('start-game').disabled = false;
+      this._initializeSquares();
+      this._show();
+    }
+
+    /**
+     * Indicates whether the puzzle is solved.
+     * @returns {boolean} true if the puzzle is solved.
+     */
+    isSolved() {
+      return this._ordinates.filter(
+          (value, index) => value !== index + 1).length === 0;
+    }
+
+    /**
+     * Starts the game.
+     */
+    startGame() {
+      aStartTime = Date.now();
+      while (this.isSolved()) {
+        this._shuffle();
+      }
+      aNumber = setInterval(countTime, 1000);
+      document.getElementById('start-game').disabled = true;
+    }
+  }
 
   /**
    * Renews the timer on the page.
@@ -399,4 +393,11 @@
         `:${minute < 10 ? 0 : ''}${minute}` +
         `:${second < 10 ? 0 : ''}${second}`;
   }
+
+  window.addEventListener('load', () => {
+    const board = new Board();
+    const button = document.getElementById('start-game');
+    button.addEventListener('click', () => board.startGame());
+    ScorePad.show();
+  });
 })();
