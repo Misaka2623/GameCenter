84496be77981c606c9c68c83ec312e69e8c6c34c
compasslg <lihanwei4c@gmail.com>
2019-03-30 18:48:22 -0700

    test sql query

--- sql_connection.js
+++ sql_connection.js
@@ -2,7 +2,8 @@ var mysql	= require('mysql');
 var connection	= mysql.createConnection({
 	host	:	'csc346-project2.c3jmudlmcgqc.us-east-1.rds.amazonaws.com',
 	user	:	'lihanwei4c',
-	password:	'gamecenterdbpassword'
+	password:	'gamecenterdbpassword',
+	database:	'GameInfo'
 });
 
 
@@ -10,3 +11,21 @@ connection.connect(function(err) {
 	if (err) throw err;
 	console.log("Connected!");
 });
+
+function login(){
+	let username = document.getElementById("username").nodeValue;
+	let password = document.getElementById("password").nodeValue;
+	console.log("username: " + username);
+	console.log("password:" + password);
+	let getRowQuery = "SELECT hashed_password FROM user_info WHERE username = " + mysql.escape(username);
+	const bcrypt = require('bcrypt');
+	const saltSeed = 10;
+	connection.query(getRowQuery, function(err, result){
+		if(err) throw err;
+		console.log(result);
+	});
+	/*
+	bcrypt.genSalt(saltSeed, function(err, salt){
+		bcrypt.hash(password, salt, )
+	});*/
+}
