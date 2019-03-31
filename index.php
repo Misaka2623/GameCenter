850854da914e69a2a5c54c9867869a20f99bcf29
compasslg <lihanwei4c@gmail.com>
2019-03-31 10:57:12 -0700

    use form for login page

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
 <meta charset="UTF-8">
 <title>Login</title>
 <body>
-    <label for="username"><input id="username"></label>
-    <label for="password"><input id="password" type="password"></label>
-    <button id="login" onclick="login()">login</button>
-    
+    <form>
+        <label for="username"><input id="username"></label><br>
+        <label for="password"><input id="password" type="password"></label><br>
+        <input type="submit" name="login">
+    </form>
 </body>
 </html>
