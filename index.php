c71fc85dacfa1931658c51313f598244b244b23f
misaka2623 <415470991@qq.com>
2019-04-03 09:35:12 -0700

    add index.css unable to test

--- index.php
+++ index.php
@@ -2,21 +2,23 @@
 <html lang="en">
 <meta charset="UTF-8">
 <title>Login</title>
+<link href="index.css" rel="stylesheet">
+
 <body>
-    <?php
-    session_start();
-    if(isset($_SESSION['user'])){
-        header('Location: fifteen_puzzle.html');
-    }
-    ?>
-    <form action="SessionController.php" method="POST" align="center">
-        <label for="username">username:</label>
-        <input id="username" type="text" name="username" required><br>
+<?php
+session_start();
+if (isset($_SESSION['user'])) {
+    header('Location: fifteen_puzzle.html');
+}
+?>
+<form action="SessionController.php" method="POST" align="center">
+  <label for="username">username:</label>
+  <input id="username" type="text" name="username" required><br>
 
-        <label for="password">password:</label>
-        <input id="password" type="password" name="password" required><br>
+  <label for="password">password:</label>
+  <input id="password" type="password" name="password" required><br>
 
-        <input type="submit" value="Login">
-    </form>
+  <input type="submit" value="Login">
+</form>
 </body>
 </html>
