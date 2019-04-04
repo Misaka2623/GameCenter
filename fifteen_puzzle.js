8614efd8e657a35e7ddf5c2845cd8214311a4438
misaka2623 <415470991@qq.com>
2019-04-04 13:46:42 -0700

    Update fifteen_puzzle.js

--- fifteen_puzzle.js
+++ fifteen_puzzle.js
@@ -459,7 +459,7 @@
   function refreshSelectableStage() {
     const value = document.getElementById('highest-level-beaten-data').innerHTML;
     console.log(value);
-    const max_stage = /^[0-9]+.?[0-9]*/.test(value) ? parseInt(value) + 1 : kMinStage;
+    const max_stage = /^[0-9]+.?[0-9]*/.test(value) ? parseInt(value) + 2 : kMinStage;
 
     const select = document.getElementById('select-stage');
     select.innerHTML = '';
@@ -468,16 +468,13 @@
       option.value = i.toString();
       option.innerHTML = (i - 1).toString();
       select.appendChild(option);
-      if (i === max_stage) {
+      if (i === Board.size + 1) {
         option.selected = true;
       }
     }
   }
 
   window.addEventListener('load', () => {
-    showUser();
-    refreshSelectableStage();
-
     const board = new Board();
     document.getElementById('start-game').
         addEventListener('click', () => board.startGame());
